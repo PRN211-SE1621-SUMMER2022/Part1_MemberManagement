36ec0a8a126dac180c7ab928bdd28927ff87124e
minhanhnguyen6266 <minhanhnguyen6266@gmail.com>
2022-07-02 22:43:05 +0700

    Reup solution

---
+++ BusinessObject/MemberObject.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessObject
+{
+    public class MemberObject
+    {
+        private string MemberID { get; set; }
+        private string MemberName { get; set; }
+        private string MemberEmail { get; set; }
+        private string Password { get; set; }
+        private string City { get; set; }
+        private string Country { get; set; }
+
+        public MemberObject()
+        {
+        }
+
+        public MemberObject(string memberID, string memberName, string memberEmail, string password, string city, string country)
+        {
+            MemberID = memberID;
+            MemberName = memberName;
+            MemberEmail = memberEmail;
+            Password = password;
+            City = city;
+            Country = country;
+        }
+
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+    }
+}
